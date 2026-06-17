7af543e7b0142ec21f19a195d61e26a62446b675
chenwei <2282049751@qq.com>
2026-06-17 10:52:35 +0800

    refactor(ccxt): 移除ccxt pro相关代码和依赖

--- ccxt.php
+++ ccxt.php
@@ -35,8 +35,8 @@ if (defined('PATH_TO_CCXT')) {
 }
 
 define('PATH_TO_CCXT', __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR);
-define('PATH_TO_WS_CCXT', __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'pro' .  DIRECTORY_SEPARATOR);
-define('PATH_TO_CCXT_ASYNC', PATH_TO_CCXT . 'async' . DIRECTORY_SEPARATOR);
+// define('PATH_TO_WS_CCXT', __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'pro' .  DIRECTORY_SEPARATOR);
+// define('PATH_TO_CCXT_ASYNC', PATH_TO_CCXT . 'async' . DIRECTORY_SEPARATOR);
 
 spl_autoload_register(function ($class) {
     // used to include static dependencies
@@ -102,10 +102,10 @@ require_once PATH_TO_CCXT . 'CancelPending.php';
 require_once PATH_TO_CCXT . 'UnsubscribeError.php';
 
 
-require_once PATH_TO_WS_CCXT . 'ClientTrait.php';
+// require_once PATH_TO_WS_CCXT . 'ClientTrait.php';
 require_once PATH_TO_CCXT . 'Precise.php';
 require_once PATH_TO_CCXT . 'Exchange.php';
-require_once PATH_TO_CCXT_ASYNC . 'Exchange.php';
+// require_once PATH_TO_CCXT_ASYNC . 'Exchange.php';
 
 
 $autoloadFile = __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
@@ -114,17 +114,17 @@ if (file_exists($autoloadFile)) {
 }
 
 spl_autoload_register(function ($class_name) {
-    $sections = explode("\\", $class_name);
-    if (in_array("ccxt\\pro", $sections)) {
-        $class_name = str_replace("ccxt\\pro\\", "", $class_name);
-        $sections = explode("\\", $class_name);
-        $class_name = str_replace("ccxt\\pro\\", "", $class_name);
-        $file = PATH_TO_WS_CCXT . $class_name . '.php';
-        if (file_exists($file)) {
-            require_once $file;
-        }
-        return;
-    }
+    // $sections = explode("\\", $class_name);
+    // if (in_array("ccxt\\pro", $sections)) {
+    //     $class_name = str_replace("ccxt\\pro\\", "", $class_name);
+    //     $sections = explode("\\", $class_name);
+    //     $class_name = str_replace("ccxt\\pro\\", "", $class_name);
+    //     $file = PATH_TO_WS_CCXT . $class_name . '.php';
+    //     if (file_exists($file)) {
+    //         require_once $file;
+    //     }
+    //     return;
+    // }
 
     $class_name = str_replace("ccxt\\", "", $class_name);
     $sections = explode("\\", $class_name);
@@ -138,14 +138,14 @@ spl_autoload_register(function ($class_name) {
 // require_once __DIR__ . DIRECTORY_SEPARATOR . 'php' . DIRECTORY_SEPARATOR . 'pro.php';
 
 
-namespace ccxt\pro;
+// namespace ccxt\pro;
 
-require_once PATH_TO_WS_CCXT . 'Future.php';
-require_once PATH_TO_WS_CCXT . 'Client.php';
-require_once PATH_TO_WS_CCXT . 'OrderBook.php';
-require_once PATH_TO_WS_CCXT . 'OrderBookSide.php';
-require_once PATH_TO_WS_CCXT . 'BaseCache.php';
-require_once PATH_TO_WS_CCXT . 'ArrayCache.php';
-require_once PATH_TO_WS_CCXT . 'ArrayCacheByTimestamp.php';
-require_once PATH_TO_WS_CCXT . 'ArrayCacheBySymbolById.php';
-require_once PATH_TO_WS_CCXT . 'Exchange.php';
+// require_once PATH_TO_WS_CCXT . 'Future.php';
+// require_once PATH_TO_WS_CCXT . 'Client.php';
+// require_once PATH_TO_WS_CCXT . 'OrderBook.php';
+// require_once PATH_TO_WS_CCXT . 'OrderBookSide.php';
+// require_once PATH_TO_WS_CCXT . 'BaseCache.php';
+// require_once PATH_TO_WS_CCXT . 'ArrayCache.php';
+// require_once PATH_TO_WS_CCXT . 'ArrayCacheByTimestamp.php';
+// require_once PATH_TO_WS_CCXT . 'ArrayCacheBySymbolById.php';
+// require_once PATH_TO_WS_CCXT . 'Exchange.php';
